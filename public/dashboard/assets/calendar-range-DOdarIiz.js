2b6774f1e200de86e7c0516679d61771f4325d6d
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2026-04-14 00:38:53 -0600

    solucion pproblema punto 2.2

--- public/dashboard/assets/calendar-range-DOdarIiz.js
+++ public/dashboard/assets/calendar-range-DOdarIiz.js
@@ -1,4 +1,4 @@
-import{ai as e}from"./index-DVHIy21N.js";/**
+import{ai as e}from"./index-Dyf4970f.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
