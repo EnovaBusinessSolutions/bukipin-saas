a2ba3fbea708f4090fb506bfae8bfdeaea64a2e4
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2026-04-14 21:58:56 -0600

    modificacion total del css del nuevo panel analitica financiamiento

--- public/dashboard/assets/calendar-range-CQk4bYLV.js
+++ public/dashboard/assets/calendar-range-CQk4bYLV.js
@@ -1,4 +1,4 @@
-import{ai as e}from"./index-C4dZvVOU.js";/**
+import{ai as e}from"./index-WCwXLxcy.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
